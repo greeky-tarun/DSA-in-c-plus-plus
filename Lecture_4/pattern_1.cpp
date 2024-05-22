2c995cf1b5327ec91fe479302b9d07b12a28ac74
B. Tarun Kumar <btarunkumar7010@gmail.com>
2024-05-22 16:54:03 +0530

    Pattern

---
+++ Lecture_4/pattern_1.cpp
@@ -0,0 +1,16 @@
+// rectangular pattern
+#include <iostream>
+using namespace std;
+int main()
+{
+    int r, c;
+    cin >> r >> c;
+    for (int i = 1; i <= r; i++)
+    {
+        for (int j = 1; j <= r; j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
